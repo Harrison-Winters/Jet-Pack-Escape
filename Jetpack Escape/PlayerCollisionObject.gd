998a351e5c720832b8f002b353ca7433987837b4
Harrison-Winters <winters23@up.edu>
2022-09-08 18:45:27 -0700

    Implemented Basic Collision
    Used move_and_collide method to implement collision

--- Jetpack Escape/PlayerCollisionObject.gd
+++ Jetpack Escape/PlayerCollisionObject.gd
@@ -7,16 +7,37 @@ func _ready():
 	pass # Replace with function body.
 
 
-func _process(delta):
+#func _process(delta):
+#	if Input.is_action_pressed("ui_left"):
+#		move_and_collide()
+#
+#		var velocity = Vector2.LEFT * 400
+#		position += velocity * delta
+#	if Input.is_action_pressed("ui_right"):
+#		var velocity = Vector2.RIGHT * 400
+#		position += velocity * delta
+#	if Input.is_action_pressed("ui_up"):
+#		var velocity = Vector2.UP * 400
+#		position += velocity * delta	
+#	if Input.is_action_pressed("ui_down"):
+#		var velocity = Vector2.DOWN * 400
+#		position += velocity * delta	
+
+func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
+		
 		var velocity = Vector2.LEFT * 400
-		position += velocity * delta
+		move_and_collide(velocity * delta)
+#		position += velocity * delta
 	if Input.is_action_pressed("ui_right"):
 		var velocity = Vector2.RIGHT * 400
-		position += velocity * delta
+		move_and_collide(velocity * delta)
+#		
 	if Input.is_action_pressed("ui_up"):
 		var velocity = Vector2.UP * 400
-		position += velocity * delta	
+		move_and_collide(velocity * delta)
+#		
 	if Input.is_action_pressed("ui_down"):
 		var velocity = Vector2.DOWN * 400
-		position += velocity * delta	
+		move_and_collide(velocity * delta)
+#		
