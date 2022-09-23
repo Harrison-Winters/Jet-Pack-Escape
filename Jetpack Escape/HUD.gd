f506671246af8483099f5f4be3ecfa20f87b1f39
Harrison-Winters <winters23@up.edu>
2022-09-23 11:03:36 -0700

    Temp

--- Jetpack Escape/HUD.gd
+++ Jetpack Escape/HUD.gd
@@ -30,3 +30,7 @@ func _on_StartButton_pressed():
 	$LifeCounter.show()
 	emit_signal("start_game")
 	
+
+
+func _on_Part1_win():
+	
--- Jetpack Escape/WinArea.gd
+++ Jetpack Escape/WinArea.gd
@@ -3,17 +3,16 @@ extends Area2D
 signal win
 
 var velocity = Vector2.ZERO
-# asdasd
+
 
 func _ready():
 	pass
 	
 func _process(delta):
-#	var velocity = Vector2.DOWN * 200
+
 	position += velocity * delta
 	
-#	if position.y > 800:
-#		position.y = -800
+
 	
 func update_velocity():
 	velocity = Vector2.DOWN * 200
