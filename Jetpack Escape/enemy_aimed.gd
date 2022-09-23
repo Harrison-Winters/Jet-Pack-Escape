446a4016f1959d7cb1385697a4c7ae5f23b29e62
ReynHisamoto <hisamoto24@up.edu>
2022-09-23 12:16:29 -0700

    github restore

--- Jetpack Escape/enemy_aimed.gd
+++ Jetpack Escape/enemy_aimed.gd
@@ -12,6 +12,5 @@ func get_player_position():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	get_player_position()
-	if aimPosition != null:
-		look_at(aimPosition)
+	look_at(aimPosition)
 	pass
--- Jetpack Escape/enemy_bullet.gd
+++ Jetpack Escape/enemy_bullet.gd
@@ -4,22 +4,15 @@ extends "res://bullet.gd"
 # Declare member variables here. Examples:
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#angle = global_rotation
 	angle = PI
-	speed = 350.0
 	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _process(delta):
 	var velocity = Vector2.UP.rotated(angle) * speed
-	#var velocity  = global_transform.x * speed
 	position += velocity * delta
 	
 #destroys bullet once it exits the screen
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
-
-func _on_EnemyBullet_body_entered(body):
-	if body is Player:
-		body.damage(1)
