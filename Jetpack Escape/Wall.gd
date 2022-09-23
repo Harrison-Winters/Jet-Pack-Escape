34f85776e013b48e3e657c4ad574bec967b6a6a8
ReynHisamoto <hisamoto24@up.edu>
2022-09-22 18:56:32 -0700

    stage building

---
+++ Jetpack Escape/Wall.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+
+func _ready():
+	pass # Replace with function body.
+	
+func _process(delta):
+	var velocity = Vector2.DOWN * 200
+	position += velocity * delta	
+	
+	if position.y > 800:
+		position.y = -800
+	
