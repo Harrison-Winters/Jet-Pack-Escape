3d655dead06c8396ff57e472ffb84b25fcd05541
deepseaisopod <marshak22@up.edu>
2022-09-22 18:09:04 -0700

    Update enemy_bullet.gd

--- Jetpack Escape/enemy_bullet.gd
+++ Jetpack Escape/enemy_bullet.gd
@@ -1,21 +1,16 @@
-extends Area2D
+extends "res://bullet.gd"
 
 
 # Declare member variables here. Examples:
-var angular_speed = PI
-var speed = 400.0
-var angle = 0
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#TODO: set angle to be the rotation angle of parent (player or enemy)
-	pass # Replace with function body.
+	angle = PI
+	#pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.DOWN.rotated(angle) * speed
+	var velocity = Vector2.UP.rotated(angle) * speed
 	position += velocity * delta
 	
 #destroys bullet once it exits the screen
