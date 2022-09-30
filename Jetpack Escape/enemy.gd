0b64e131908894086d0129a7c638e4bc35ecea35
ReynHisamoto <hisamoto24@up.edu>
2022-09-29 18:46:49 -0700

    added enemies to stage

--- Jetpack Escape/enemy.gd
+++ Jetpack Escape/enemy.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 class_name Enemy
 
 var life = 4
+var velocity = Vector2.ZERO
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,6 +13,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	position += velocity * delta
 	pass
 	
 func damage(amount: int):
@@ -28,3 +30,6 @@ func _on_ShotTimer_timeout():
 	var bullet = resource.instance()
 	owner.add_child(bullet)
 	bullet.transform = $EnemyShootPosition.global_transform
+	
+func update_velocity():
+	velocity = Vector2.DOWN * 200
